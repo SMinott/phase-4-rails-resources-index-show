3c333cda66f2a05756f3a946fd31656e8ca1f361
Susan Minott <susan_minott@yahoo.com>
2022-06-28 19:02:11 -0400

    commit tree aed76b56a7c26c0084f62921d3b15ffdcda5f09a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+  #Instead of using the 2 lines of code above:
+  resources :birds, only: [:index, :show]
 end
